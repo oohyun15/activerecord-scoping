7ddd3b8c2446eba62833795cacd4c1a285677064
oohyun15 <sakiss4774@gmail.com>
2022-03-22 01:00:22 +0900

    Edit spec

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -13,24 +13,20 @@ describe 'ActiveRecord', type: :model do
   context 'with where clause' do
     it "doesn't return expected query if where clause exists before scope" do
       expected_query = <<-SQL.squish
-        SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE `posts`.`type` = 'Post::Normal' AND (`posts`.`status` = 'open' OR `comments`.`status` = 'open')
+        SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE `posts`.`id` IN (1, 2, 3) AND (`posts`.`status` = 'open' OR `comments`.`status` = 'open')
       SQL
 
-      expect(Post.where(type: 'Post::Normal').scope1.to_sql).to eq expected_query
-      expect(Post.where(type: 'Post::Normal').scope2.to_sql).to eq expected_query # SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE (`posts`.`type` = 'Post::Normal' AND `posts`.`status` = 'open' OR `comments`.`status` = 'open')
-
-      # same queries
-      # expect(Post::Normal.scope1.to_sql).to eq expected_query
-      # expect(Post::Normal.scope2.to_sql).not_to eq expected_query # error
+      expect(Post.where(id: [1 , 2, 3]).scope1.to_sql).to eq expected_query
+      expect(Post.where(id: [1 , 2, 3]).scope2.to_sql).to eq expected_query
     end
 
     it 'returns expected query if where clause exists after scope' do
       expected_query = <<-SQL.squish
-      SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE (`posts`.`status` = 'open' OR `comments`.`status` = 'open') AND `posts`.`type` = 'Post::Normal'
+        SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE (`posts`.`status` = 'open' OR `comments`.`status` = 'open') AND `posts`.`id` IN (1, 2, 3)
       SQL
 
-      expect(Post.scope1.where(type: 'Post::Normal').to_sql).to eq expected_query
-      expect(Post.scope2.where(type: 'Post::Normal').to_sql).to eq expected_query
+      expect(Post.scope1.where(id: [1 , 2, 3]).to_sql).to eq expected_query
+      expect(Post.scope2.where(id: [1 , 2, 3]).to_sql).to eq expected_query
     end
   end
 end
