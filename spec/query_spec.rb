2dd5336c77c25e25b767675e9c8f7b89787e6207
oohyun15 <sakiss4774@gmail.com>
2022-03-22 01:04:36 +0900

    Edit spec

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -20,6 +20,15 @@ describe 'ActiveRecord', type: :model do
       expect(Post.where(id: [1 , 2, 3]).scope2.to_sql).to eq expected_query
     end
 
+    it 'returns expected query if where clause exists before scope but using merge method' do
+      expected_query = <<-SQL.squish
+        SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE `posts`.`id` IN (1, 2, 3) AND (`posts`.`status` = 'open' OR `comments`.`status` = 'open')
+      SQL
+
+      expect(Post.where(id: [1 , 2, 3]).scope1.to_sql).to eq expected_query
+      expect(Post.where(id: [1 , 2, 3]).merge(Post.scope2).to_sql).to eq expected_query
+    end
+
     it 'returns expected query if where clause exists after scope' do
       expected_query = <<-SQL.squish
         SELECT `posts`.* FROM `posts` LEFT OUTER JOIN `comments` ON `comments`.`post_id` = `posts`.`id` WHERE (`posts`.`status` = 'open' OR `comments`.`status` = 'open') AND `posts`.`id` IN (1, 2, 3)
