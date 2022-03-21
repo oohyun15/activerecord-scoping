6151101f3712d17af3f25bd828490c0bff3cbd1e
oohyun15 <sakiss4774@gmail.com>
2022-03-22 00:58:00 +0900

    more simple

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,15 @@
 class Post < ApplicationRecord
   has_many :comments
 
-  scope :available, -> { where(status: 'open') }
-
   scope :scope1, -> {
     left_joins(:comments)
-    .available
+    .where(status: 'open')
     .or(where(comments: { status: 'open' }))
   }
 
   scope :scope2, -> {
     left_joins(:comments)
-    .available
-    .or(Comment.available)
+    .where(status: 'open')
+    .or(Comment.where(status: 'open'))
   }
-
-  class Post::Normal < Post; end
 end
